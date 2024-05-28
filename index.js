6e1f31b33b4ad696eb7159aacff9db020c48aff8
nedoratic <nedoratic@gmail.com>
2024-05-28 08:05:15 +0200

    index.js - get random computer result function added

--- index.js
+++ index.js
@@ -8,3 +8,10 @@ const resetGameBtn = document.getElementById('reset-game-btn');
 const rockBtn = document.getElementById('rock-btn');
 const paperBtn = document.getElementById('paper-btn');
 const scissorsBtn = document.getElementById('scissors-btn');
+
+// Get random computer result
+function getRandomComputerResult() {
+	const options = ['Rock', 'Paper', 'Scissors'];
+	const randomIndex = Math.floor(Math.random() * options.length);
+	return options[randomIndex];
+}
